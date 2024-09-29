eb985d919eb28ec52cd6ca49534448bf133bf767
Humberto Uchiyama Costa <humbertou.costa@gmail.com>
2024-09-28 22:03:20 -0300

    implements utility dependencies

--- app/src/main/java/com/humberto/tasky/main/TaskyApp.kt
+++ app/src/main/java/com/humberto/tasky/main/TaskyApp.kt
@@ -1,7 +1,18 @@
 package com.humberto.tasky.main
 
 import android.app.Application
+import com.humberto.tasky.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class TaskyApp: Application()
+class TaskyApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree()) // Plant a tree for logging in debug mode
+        }
+    }
+}
