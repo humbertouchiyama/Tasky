d35618f006a31a4775933564291677e89672c8aa
Humberto Uchiyama Costa <humbertou.costa@gmail.com>
2024-10-03 22:24:25 -0300

    removes accompanist, adds field focus changes, adds scaffold

--- app/src/main/java/com/humberto/tasky/core/presentation/designsystem/components/TaskyTextField.kt
+++ app/src/main/java/com/humberto/tasky/core/presentation/designsystem/components/TaskyTextField.kt
@@ -24,16 +24,16 @@ import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,10 +50,9 @@ fun TaskyTextField(
     modifier: Modifier = Modifier,
     hasError: Boolean? = false,
     keyboardType: KeyboardType = KeyboardType.Text,
+    isFocused: MutableState<Boolean>,
+    focusRequester: FocusRequester
 ) {
-    var isFocused by remember {
-        mutableStateOf(false)
-    }
     Column(
         modifier = modifier
     ) {
@@ -63,16 +62,15 @@ fun TaskyTextField(
                 color = MaterialTheme.colorScheme.onSurfaceVariant
             ),
             keyboardOptions = KeyboardOptions(
-                keyboardType = keyboardType
+                keyboardType = keyboardType,
             ),
             lineLimits = TextFieldLineLimits.SingleLine,
-            cursorBrush = SolidColor(MaterialTheme.colorScheme.onTertiary),
             modifier = Modifier
                 .clip(RoundedCornerShape(16.dp))
                 .background(MaterialTheme.colorScheme.tertiary)
                 .border(
-                    width = if(isFocused || hasError == true) 1.dp else 0.dp,
-                    color = if(isFocused) {
+                    width = if(isFocused.value || hasError == true) 1.dp else 0.dp,
+                    color = if(isFocused.value) {
                         TaskyLightBlue
                     } else if (hasError == true) {
                         MaterialTheme.colorScheme.error
@@ -82,8 +80,9 @@ fun TaskyTextField(
                     shape = RoundedCornerShape(16.dp)
                 )
                 .padding(horizontal = 20.dp)
+                .focusRequester(focusRequester)
                 .onFocusChanged {
-                    isFocused = it.isFocused
+                    isFocused.value = it.isFocused
                 }
                 .defaultMinSize(minHeight = 64.dp),
             decorator = { innerBox ->
@@ -95,7 +94,7 @@ fun TaskyTextField(
                         modifier = Modifier
                             .weight(1f)
                     ) {
-                        if (state.text.isEmpty() && !isFocused) {
+                        if (state.text.isEmpty() && !isFocused.value) {
                             Text(
                                 text = hint,
                                 color = MaterialTheme.colorScheme.onSurfaceVariant.copy(
@@ -128,14 +127,18 @@ fun TaskyTextField(
 @Preview
 @Composable
 private fun TaskyTextFieldPreview() {
+    val isFocusedState = remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+
     TaskyTheme {
         TaskyTextField(
             state = rememberTextFieldState(),
             endIcon = Icons.Default.Check,
             hint = "Email address",
             modifier = Modifier
-                .fillMaxWidth()
-
+                .fillMaxWidth(),
+            isFocused = isFocusedState,
+            focusRequester = focusRequester
         )
     }
 }
