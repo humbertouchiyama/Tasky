8703e59ca8677a7acddee54afa1f0e572736354d
Humberto Uchiyama Costa <humbertou.costa@gmail.com>
2024-10-06 17:40:17 -0300

    implements pr review adjustments, separates register form fields state collectors

--- app/src/main/java/com/humberto/tasky/auth/presentation/registration/RegisterState.kt
+++ app/src/main/java/com/humberto/tasky/auth/presentation/registration/RegisterState.kt
@@ -5,12 +5,12 @@ import com.humberto.tasky.auth.domain.PasswordValidationState
 
 data class RegisterState(
     val fullName: TextFieldState = TextFieldState(),
-    val isFullNameValid: Boolean = false,
+    val isValidFullName: Boolean = false,
     val email: TextFieldState = TextFieldState(),
-    val isEmailValid: Boolean = false,
+    val isValidEmail: Boolean = false,
     val password: TextFieldState = TextFieldState(),
     val passwordValidationState: PasswordValidationState = PasswordValidationState(),
     val isPasswordVisible: Boolean = false,
     val canRegister: Boolean = false,
-    val isRegisteringIn: Boolean = false,
+    val isRegistering: Boolean = false,
 )
